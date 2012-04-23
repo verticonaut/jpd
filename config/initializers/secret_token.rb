c27c1f83528bdc9716d46eb86bfca3fcc351f57f
Martin Schweizer <contact@verticonaut.me>
2012-04-23 22:02:19 +0200

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jpd::Application.config.secret_token = '001bbbe8bf2e569162f750af83101616e49d188e9af9c892463deba8c949fbea8f55a9512961ee8e442564f0e05e7c9ba5994c90ecca05b9f1524a7cf0268e63'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Jpd::Application.routes.draw do
+  root :to => "home#index"
+end
